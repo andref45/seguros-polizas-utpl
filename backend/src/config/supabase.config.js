63451b54eb6cf2968e1bbb09119a55ed16d13ecb
alexfer060900 <alexaguirrerojas@hotmail.com>
2026-01-06 18:05:22 -0500

    feat: Mejora login

--- backend/src/config/supabase.config.js
+++ backend/src/config/supabase.config.js
@@ -6,6 +6,12 @@ dotenv.config()
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY
 
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log(
+  'SUPABASE_SERVICE_KEY:',
+  process.env.SUPABASE_SERVICE_KEY ? '*** definida ***' : 'NO definida'
+);
+
 if (!supabaseUrl || !supabaseServiceKey) {
   throw new Error('SUPABASE_URL y SUPABASE_SERVICE_KEY son requeridos en .env')
 }
@@ -19,3 +25,4 @@ export const supabase = createClient(supabaseUrl, supabaseServiceKey, {
 })
 
 export default supabase
+
